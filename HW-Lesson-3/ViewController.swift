83e2efb778f9ff959c664313d06072b15637306d
Vladislav <vlad-sachok@mail.ru>
2026-04-22 03:21:23 +0300

    Initial Commit

---
+++ HW-Lesson-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-Lesson-3
+//
+//  Created by Vladislav Sachok on 22.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

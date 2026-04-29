c2f479f3a28089f0e914ef87392a9e71fe950f6c
Vladislav <vlad-sachok@mail.ru>
2026-04-29 19:44:12 +0300

    ADDED

--- HW-Lesson-3/AppDelegate.swift
+++ HW-Lesson-3/AppDelegate.swift
@@ -9,7 +9,7 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-let number = 1
+
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
